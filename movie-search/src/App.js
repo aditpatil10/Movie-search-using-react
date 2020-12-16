fb627478be374a2f8c89aec6f72ba2602ead98f8
aditpatil10 <adit_patil@yahoo.com>
2020-12-17 00:23:20 +0530

    Add[Basic Setup]

---
+++ movie-search/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">React Movie search</h1>
+    </div>
+  );
+}
+
+export default App;
